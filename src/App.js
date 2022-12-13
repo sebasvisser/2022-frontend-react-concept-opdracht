780ac2c647a3e808c84a0b57a1d67e703374d6cb
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-12-13 22:44:02 +0100

    pagina's zelf geimporteerd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route }
+    from 'react-router-dom';
+import Home
+    from './pages/home/Home';
+import Subreddit
+    from "./pages/subreddit/Subreddit";
 
 function App() {
   return (
