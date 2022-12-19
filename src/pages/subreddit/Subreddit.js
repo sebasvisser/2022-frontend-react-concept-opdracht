53f8b03d9f7dd5821f6d9beaf0b7d27000e8bd8e
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-12-19 20:50:30 +0100

    yes, subreddit pagina werkend

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -12,11 +12,23 @@ import axios
 
 function Subreddit() {
     const { id } = useParams();
+    console.log("hoi");
+    console.log(id);
+    console.log("doei");
 
     async function getSubredditInfo(subreddit) {
         try {
             const response = await axios.get(`https://www.reddit.com/r/${subreddit}/about.json`);
             const data = response.data;
+            // Get a reference to the elements where you want to display the data
+            const subredditNameElement = document.getElementById('subreddit-name');
+            const subredditDescriptionElement = document.getElementById('subreddit-description');
+            const subredditSubscribersElement = document.getElementById('subreddit-subscribers');
+
+            // Set the innerHTML of the elements to the corresponding data
+            subredditNameElement.innerHTML = data.data.display_name;
+            subredditDescriptionElement.innerHTML = data.data.public_description;
+            subredditSubscribersElement.innerHTML = data.data.subscribers;
             console.log(`Subreddit: ${data.data.display_name}`);
             console.log(`Description: ${data.data.public_description}`);
             console.log(`Subscribers: ${data.data.subscribers}`);
@@ -25,14 +37,18 @@ function Subreddit() {
         }
     }
 
-    getSubredditInfo('javascript');
+    getSubredditInfo(id);
 
     return (
         <>
+            <h2>Title</h2>
+            <p id="subreddit-name"></p>
+            <h2>Description</h2>
+            <p id="subreddit-description"></p>
+            <h2>Number of Subscribers</h2>
+            <p id="subreddit-subscribers"></p>
             <div>Het Subreddit is {id}</div>
-            <p>We gaan aanroepen vanuit:</p>
-            <p>GET https://www.reddit.com/r/{id}/about.json</p>
-            <p>Hoi vanuit de Subredditz</p>
+
             <NavLink to="/">"Take me back</NavLink>
         </>
 
