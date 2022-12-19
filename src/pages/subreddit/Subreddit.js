3c5df2e6c3ddc458918e02fc2a333ad733653491
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-12-19 20:40:07 +0100

    met hulp van chatgtp axios functie gemaakt die werkt

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {
+    useEffect,
+    useState
+} from 'react';
 import './Subreddit.css';
 import {
     NavLink,
     useParams
 } from "react-router-dom";
+import axios
+    from "axios";
 
 function Subreddit() {
     const { id } = useParams();
+
+    async function getSubredditInfo(subreddit) {
+        try {
+            const response = await axios.get(`https://www.reddit.com/r/${subreddit}/about.json`);
+            const data = response.data;
+            console.log(`Subreddit: ${data.data.display_name}`);
+            console.log(`Description: ${data.data.public_description}`);
+            console.log(`Subscribers: ${data.data.subscribers}`);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    getSubredditInfo('javascript');
 
     return (
         <>
